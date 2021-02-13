2a0f97e7161b0b09a703e861483c166240fa8bad
Alison Silva <alison.dev.silva@gmail.com>
2021-02-12 23:06:40 -0300

    Map entities

--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -6,24 +6,37 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity(name = "request_stage")
 public class RequestStage {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "text")
     private String description;
 
+    @Column(name = "realization_date")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date realizationDate;
 
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestState state;
 
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
     private Request request;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 }
--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -2,11 +2,9 @@ package com.springcourse.domain;
 
 
 import com.springcourse.domain.enums.Role;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,19 +12,29 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 75, nullable = false)
     private String name;
 
+    @Column(length = 75, nullable = false, unique = true)
     private String email;
 
+    @Column(length = 100, nullable = false)
     private String password;
 
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
 
+    @OneToMany(mappedBy = "user")
     private List<Request> requests = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user")
     private List<RequestStage> stages = new ArrayList<>();
 }
